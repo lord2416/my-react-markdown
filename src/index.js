45ecb132c9c7c0bbe77b73734d864a7bd6a127b9
Lord2416 <lord2416@qq.com>
2018-04-03 22:41:22 +0800

    develop

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './component/App';
 import registerServiceWorker from './registerServiceWorker';
-import './style/css/main.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
